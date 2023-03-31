8b986370f0b00e7f3d30f5ad1c553c892172eb1a
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-31 16:08:50 +0700

    selesai melakukan perbandingan i<j

--- QuickShort_065/QuickShort_065.cpp
+++ QuickShort_065/QuickShort_065.cpp
@@ -44,5 +44,51 @@ void q_short(int low, int high)
 	int pivot, i, j;
 	if (low > high)//langkah 1
 		return;
+	//partition the list into two parts
+	//one containing element less that or aqual to pivot
+	//outher containing element greather than pivot
+
+	pivot = arr[low];//lagkah 2
+
+	i = low + 1;//langkah 3
+	j = high;// langkah 4
+
+	while (i <= j)//langkah 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))//langkah 7
+		{
+			i++;//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;//search for an element greater than pivot
+		while ((arr[j] > pivot) && (j >= low))//langkah 7
+		{
+			j--;//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j)//langkah 9
+			//if the greater element is in the left of the element 
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	// j now containt the index of the ast element in the sorted list
+	if (low < j)//langkah 11
+	{
+		//move the pivot to ist correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot uising quck sort
+	q_short(low, j - 1);//langkah 12
+
+	// sort teh list on the right of pivot using quick sort
+	q_short(j + i, high);//langkah 13
+}
+
 
 	
