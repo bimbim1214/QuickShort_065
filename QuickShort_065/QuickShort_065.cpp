6e41b9552aa4ba08a6663876c46bff9d6ecddad7
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-31 15:56:36 +0700

    membuat void input

--- QuickShort_065/QuickShort_065.cpp
+++ QuickShort_065/QuickShort_065.cpp
@@ -7,3 +7,20 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movement
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "masukan panjanng element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukan panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n-------------------" << endl;
+
+	
